8d47946ef0757cfeb292d0652d281c82d5a7914b
Hamit Sehjal <hamitsehjal26@gmail.com>
2024-08-02 23:27:49 -0400

    Add GetOneItem UseCase

---
+++ src/use-cases/pantry-management/getPantryItems/GetOnePantryItemUseCase.ts
@@ -0,0 +1,30 @@
+import { FirestoreService } from '@/services/FirestoreService';
+import {
+  GetPantryItemsOptions,
+  GetPantryItemsResponse,
+  PantryItemResponse,
+} from '@/use-cases/pantry-management/getPantryItems/contract/GetPantryItemsContract';
+import { StorageService } from '@/services/StorageService';
+
+export class GetOnePantryItemUseCase {
+  // constructor
+  constructor(
+    private firestoreService: FirestoreService,
+    private storageService: StorageService
+  ) {}
+
+  // getPantryItems
+  async execute(itemId: string): Promise<PantryItemResponse | null> {
+    const pantryItem = await this.firestoreService.getItem(itemId);
+
+    if (!pantryItem) {
+      throw new Error(`No Item for ${itemId} exits`);
+    }
+
+    let imageURL: string = 'shared_assets';
+    if (pantryItem.imagePath) {
+      imageURL = await this.storageService.getImageURL(pantryItem.imagePath);
+    }
+    return new PantryItemResponse(pantryItem, imageURL);
+  }
+}
